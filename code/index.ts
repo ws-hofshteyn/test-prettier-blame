73231c359c42f22bba3a55080fb07592e2c48fae
Ilya Gofshteyn <ilya.gofshtein_external@wartsila.com>
2021-08-26 15:52:58 +0300

    formatted code ATTENTION!!!!

--- code/index.ts
+++ code/index.ts
@@ -1,13 +1,13 @@
-import { reaction } from "mobx"
-import { isMobileApp } from "../../../utils/globals"
+import { reaction } from 'mobx';
+import { isMobileApp } from '../../../utils/globals';
 
 interface Serialized {
-    [key: string]: string
+    [key: string]: string;
 }
 
 export interface WithState {
-    deserialize(v: Serialized): void
-    serialize(): Serialized
+    deserialize(v: Serialized): void;
+    serialize(): Serialized;
 }
 
 /**
@@ -15,27 +15,27 @@ export interface WithState {
  * @returns {Serialized}
  */
 export function getStateFromLocation(): Serialized {
-    let location = window.location.href
-    let state: Serialized = {}
-    const p = location.split("?", -1)
-    const paths = p[0].split('/', -1)
-    const lastPath = paths[paths.length - 1]
+    let location = window.location.href;
+    let state: Serialized = {};
+    const p = location.split('?', -1);
+    const paths = p[0].split('/', -1);
+    const lastPath = paths[paths.length - 1];
     if (/live|weather|overview/.test(lastPath)) {
-        state['mode'] = lastPath === 'live' ? 'notifications' : 'weather'
+        state['mode'] = lastPath === 'live' ? 'notifications' : 'weather';
     }
     if (p.length > 1) {
-        let hashPart = p[1]
-        let items = hashPart.split('&', -1)
+        let hashPart = p[1];
+        let items = hashPart.split('&', -1);
         for (let i of items) {
-            const ix = i.indexOf('=')
+            const ix = i.indexOf('=');
             if (ix >= 0) {
-                const k = i.slice(0, ix)
-                const v = i.slice(ix + 1).split('#')[0]
-                state[k] = decodeURIComponent(v)
+                const k = i.slice(0, ix);
+                const v = i.slice(ix + 1).split('#')[0];
+                state[k] = decodeURIComponent(v);
             }
         }
     }
-    return state
+    return state;
 }
 
 /**
@@ -44,61 +44,70 @@ export function getStateFromLocation(): Serialized {
  * state from the URL.
  */
 export function setupBrowserHistory(appState: WithState) {
-    let updateUrl = true
-    let popState = true
+    let updateUrl = true;
+    let popState = true;
 
     function applyStateFromLocation() {
-        updateUrl = false
+        updateUrl = false;
         try {
-            const state = getStateFromLocation()
+            const state = getStateFromLocation();
             if (Object.keys(state).length > 0) {
-                appState.deserialize(getStateFromLocation())
+                appState.deserialize(getStateFromLocation());
             }
         } finally {
-            updateUrl = true
+            updateUrl = true;
         }
     }
 
-    reaction((): Serialized => {
-        return appState.serialize()
-    }, (newValue) => {
-        if (!updateUrl || !newValue) {
-            return
-        }
-        const state = getStateFromLocation()
-        if (state['mode']) {
-            delete state['mode']
-        }
-        const oldState = Object.assign({}, state)
-        for (let k of Object.keys(newValue)) {
-            state[k] = newValue[k]
-        }
-        for (let key in state as any) {
-            if (!newValue[key]) {
-                delete state[key]
+    reaction(
+        (): Serialized => {
+            return appState.serialize();
+        },
+        (newValue) => {
+            if (!updateUrl || !newValue) {
+                return;
             }
-        }
-        const p = window.location.href.split("?", -1)
-        popState = false
-        try {
-            const func = Object.keys(oldState).length > 0 ? 'pushState' : 'replaceState'
-            const newUrl = p[0] + '?' + Object.keys(state).map(k => `${k}=${encodeURIComponent(state[k])}`).join("&")
-            if (newUrl !== window.location.href) {
-                history[func]({}, '', newUrl)
+            const state = getStateFromLocation();
+            if (state['mode']) {
+                delete state['mode'];
             }
-        } finally {
-            popState = true
+            const oldState = Object.assign({}, state);
+            for (let k of Object.keys(newValue)) {
+                state[k] = newValue[k];
+            }
+            for (let key in state as any) {
+                if (!newValue[key]) {
+                    delete state[key];
+                }
+            }
+            const p = window.location.href.split('?', -1);
+            popState = false;
+            try {
+                const func = Object.keys(oldState).length > 0 ? 'pushState' : 'replaceState';
+                const newUrl =
+                    p[0] +
+                    '?' +
+                    Object.keys(state)
+                        .map((k) => `${k}=${encodeURIComponent(state[k])}`)
+                        .join('&');
+                if (newUrl !== window.location.href) {
+                    history[func]({}, '', newUrl);
+                }
+            } finally {
+                popState = true;
+            }
+        },
+        {
+            delay: 200
         }
-    }, {
-        delay: 200
-    })
+    );
 
     window.onpopstate = () => {
         if (!popState) {
-            return
+            return;
         }
-        applyStateFromLocation()
-    }
+        applyStateFromLocation();
+    };
 
-    applyStateFromLocation()
+    applyStateFromLocation();
 }
